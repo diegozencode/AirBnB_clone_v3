c7ba35479686248e310c5eb5531dad7f7ca53701
Diego Monroy <difemo91@gmail.com>
2020-01-29 22:18:08 +0000

    [TEST] add test for get and count db

--- tests/test_models/test_engine/test_db_storage.py
+++ tests/test_models/test_engine/test_db_storage.py
@@ -86,3 +86,11 @@ class TestFileStorage(unittest.TestCase):
     @unittest.skipIf(models.storage_t != 'db', "not testing db storage")
     def test_save(self):
         """Test that save properly saves objects to file.json"""
+
+    @unittest.skipIf(models.storage_t != 'db', "not testing db storage")
+    def test_db_get_method(self):
+        """Test that get one object"""
+
+    @unittest.skipIf(models.storage_t != 'db', "not testing db storage")
+    def test_db_count_method(self):
+        """Test that count object class"""
